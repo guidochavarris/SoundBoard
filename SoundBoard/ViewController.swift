b681daa982238983a06be987656f6b139bcfb462
joaquin-1204 <joaquin.salas@tecsup.edu.pe>
2022-05-20 07:17:08 -0500

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Mac 08 on 20/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
